6907e6eef9e38e539c2ed17a172aa8d36096c0c1
TA <ta>
2022-05-18 14:02:52 +0200

    comments

---
+++ README.md
@@ -0,0 +1,16 @@
+Class: CST 205
+Projectname: Stock Date Analyzer
+Teammembers: Tobias Alt
+Date: 2022-05-20
+Github:
+
+How to start:
+start flask application with "flask run"
+paste url of local server in browser (e.g. http://127.0.0.1:5000) -> start site of application opens
+
+Important Info:
+The application uses the Api. The Api offers free API keys. However, the usage of the API is limited to 5 requests per second. For this reason, a Not Found site may sometimes be displayed when testing the application. In such a case just try again later.
+
+Future work:
+better search for stocks
+more analysis options for stock data charts
--- project.py
+++ project.py
@@ -1,10 +1,10 @@
 
-# Tobias Alt
+# Author:Tobias Alt
 # CST 205 Multimedia Programming and Design
-# Project
+# Project Stock Data Analyzer
 # Projectnumber 7343
-# May 19, 2022
-
+# May 20, 2022
+# GitHub: 
 
 from numerize import numerize
 from flask import Flask, render_template
@@ -17,15 +17,15 @@ import dateutil.relativedelta
 import pprint
 from numerize import numerize
 
+#API endpoint and keys
 endpoint = 'https://www.alphavantage.co/query?'
-my_keys = ['DSEC20YGZLK3JE12', '4146MARPBSQSB99J', 'DXY63PIFFPJVX3FM', '99AB07D80GEJAEJB', '77DVW67VV7G27YLF', 'QQ97SRZ0AAULXKXV' ]
+my_key = 'DSEC20YGZLK3JE12'
 
-storedaily = {}
+#data storage while server running
+datastore = {}
 
-def getRandomApiKey():
-    return random.choice(my_keys)
 
-# get html site
+# calling API with key
 def getApiData(function, symbol):
     request = endpoint + f'function={function}&symbol={symbol}'
     if function=="TIME_SERIES_INTRADAY":
@@ -34,33 +34,23 @@ def getApiData(function, symbol):
     elif function=="TIME_SERIES_DAILY":
         request += '&outputsize=full'
     
-    request += f'&apikey={getRandomApiKey()}'
-    print(request)
-    
-    try:
-        response = requests.get(request)
-        #pprint(response.json)
-        return response.json()
-    except:
-        print('error while contacting api')
+    request += f'&apikey={my_key}'
+    response = requests.get(request)
+    return response.json()
 
 
-def splitKeyName(data):
-    newdata = {}
-    for key in data:
-        newdata[key.split()[1]] = data[key]
-    return newdata
-
-
+#calculate difference between stock prices (open and close value)
 def calculateDiff(day):
     return (float(day['1. open'])-float(day['4. close'])) / float(day['1. open'])
 
+#reduce data to diff data
 def reduceToDiff(data):
     newdata = {}
     for key in data:
         newdata[key] = round(calculateDiff(data[key])*100, 2)
     return newdata
 
+#reduce data to one diff value
 def reduceToOneDiff(data):
     diff = 0.0
     numDays = 0
@@ -73,21 +63,23 @@ def reduceToOneDiff(data):
     except:
         return 0.0
     
-
+#reduce data to close stock price data
 def reduceToCloseValue(data):
     newdata = {}
     for key in data:
         newdata[key] = float(data[key]['4. close'])
     return newdata
 
-def limitDateRange(startdate, enddate, data):
+#reduce data to specific time period
+def reduceTimePeriod(startdate, enddate, data):
     newdata = {}
     for key in data:
         if (key >= startdate and key<= enddate):
             newdata[key] = data[key]
     return newdata
 
-def limitSpecificWeekday(day, data):
+#reduce data to specific weekday 
+def reduceWeekday(day, data):
     newdata = {}
     for key in data:
         ymd = key.split("-")
@@ -95,15 +87,17 @@ def limitSpecificWeekday(day, data):
             newdata[key] = data[key]
     return newdata
 
-def groupByDay(data):
+#group data by weekdays
+def groupByWeekdays(data):
     weekdays = ["Friday", "Thursday", "Wednesday", "Tuesday", "Monday"]
     newdata = {}
     for day in weekdays:
-        newdata[day] = reduceToOneDiff(limitSpecificWeekday(day, data))
+        newdata[day] = reduceToOneDiff(reduceWeekday(day, data))
 
     return newdata
 
-def limitSpecificMonth(month, data):
+#reduce Data to specific month
+def reduceMonth(month, data):
     newdata = {}
     for key in data:
         ymd = key.split("-")
@@ -111,14 +105,23 @@ def limitSpecificMonth(month, data):
             newdata[key] = data[key]
     return newdata
 
-def groupByMonth(data):
+#groyp data by months
+def groupByMonths(data):
     months = ["Dec", "Nov", "Oct", "Sep", "Aug", "Jul", "Jun", "May", "Apr", "Mar", "Feb", "Jan"]
     newdata = {}
     for month in months:
-        newdata[month] = reduceToOneDiff(limitSpecificMonth(month, data))
+        newdata[month] = reduceToOneDiff(reduceMonth(month, data))
 
     return newdata
 
+def splitKeyName(data):
+    newdata = {}
+    for key in data:
+        newdata[key.split()[1]] = data[key]
+    return newdata
+
+
+#modify data for graphjs
 def getChartData(data):
     newdata = {'labels': [], 'data': []}
     for key in data:
@@ -128,11 +131,13 @@ def getChartData(data):
     newdata['data'].reverse()
     return newdata
 
+#modify overview data  
 def modifyOverview(data):
     newdata = data
     newdata['MarketCapitalization'] =  numerize.numerize(int(newdata['MarketCapitalization']))
     return newdata
 
+#modify income statement data
 def modifyIncomeStatement(data):
     newdata = data
     for statement in range(len(data)):
@@ -144,24 +149,20 @@ def modifyIncomeStatement(data):
     return newdata
 
 
-def getAnnualData(data):
-    return data
-
-#round(getDiff(data[key])*100, 2)
-
-
-
 # create an instance of Flask
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
+#render start template
 @app.route('/')
 def start():
     return render_template('start.html')
 
+#render stock template
 @app.route('/stock/<symbol>')
 def stock(symbol):
     try:
+        #define time variables
         now = datetime.date.today()
         today = now.strftime('%Y-%m-%d')
         lastWeek = (now + dateutil.relativedelta.relativedelta(weeks=-1)).strftime('%Y-%m-%d')
@@ -169,19 +170,21 @@ def stock(symbol):
         lastYear = (now + dateutil.relativedelta.relativedelta(years=-1)).strftime('%Y-%m-%d')
         last5Year = (now + dateutil.relativedelta.relativedelta(years=-5)).strftime('%Y-%m-%d')
 
+        #get api data
         intraday = getApiData('TIME_SERIES_INTRADAY', symbol)
         daily = getApiData('TIME_SERIES_DAILY', symbol)
-        storedaily[symbol] = daily
+        datastore[symbol] = daily
         monthly = getApiData('TIME_SERIES_MONTHLY', symbol)
         overview = getApiData('OVERVIEW', symbol)
         #balancesheets = getApiData('BALANCE_SHEET', symbol)
         incomestatements = getApiData('INCOME_STATEMENT', symbol)
 
+        #prepare data before pass to template
         intradaydata = getChartData(splitKeyName(reduceToCloseValue(intraday['Time Series (5min)'])))
-        weekdata = getChartData(reduceToCloseValue(limitDateRange(lastWeek, today, daily['Time Series (Daily)'] )))
-        monthdata = getChartData(reduceToCloseValue(limitDateRange(lastMonth, today, daily['Time Series (Daily)'] )))
-        yeardata = getChartData(reduceToCloseValue(limitDateRange(lastYear, today, monthly['Monthly Time Series'] )))
-        fiveyeardata = getChartData(reduceToCloseValue(limitDateRange(last5Year, today, monthly['Monthly Time Series'] )))
+        weekdata = getChartData(reduceToCloseValue(reduceTimePeriod(lastWeek, today, daily['Time Series (Daily)'] )))
+        monthdata = getChartData(reduceToCloseValue(reduceTimePeriod(lastMonth, today, daily['Time Series (Daily)'] )))
+        yeardata = getChartData(reduceToCloseValue(reduceTimePeriod(lastYear, today, monthly['Monthly Time Series'] )))
+        fiveyeardata = getChartData(reduceToCloseValue(reduceTimePeriod(last5Year, today, monthly['Monthly Time Series'] )))
         maxdata = getChartData(reduceToCloseValue(monthly['Monthly Time Series']))
         overviewdata = modifyOverview(overview)
         #balancesheetsdata = balancesheets['annualReports']
@@ -191,22 +194,23 @@ def stock(symbol):
         return render_template('stock.html', overview = overview, data = data, symbol = symbol)
     except:
         return render_template('notAvailable.html')
-   
+
+#render advanced option chart template
 @app.route('/chart/<symbol>/<startdate>/<enddate>/<group>')
 def chart(symbol, startdate, enddate, group):
     daily = {}
     try:
-        daily = storedaily[symbol]
+        daily = datastore[symbol]
     except:
         daily = getApiData('TIME_SERIES_DAILY', symbol)
     
-    data = limitDateRange(startdate, enddate, daily['Time Series (Daily)'] )
+    data = reduceTimePeriod(startdate, enddate, daily['Time Series (Daily)'] )
     charttype = ""
     if group=="day":
-        data = getChartData(groupByDay(reduceToDiff((data))))
+        data = getChartData(groupByWeekdays(reduceToDiff((data))))
         charttype = "bar"
     elif group == "month":
-        data = getChartData(groupByMonth(reduceToDiff((data))))
+        data = getChartData(groupByMonths(reduceToDiff((data))))
         charttype = "bar"
     elif group == "none":
         data = getChartData(reduceToCloseValue((data)))
@@ -214,7 +218,7 @@ def chart(symbol, startdate, enddate, group):
 
     return render_template('chart.html',  data = {'data': data}, charttype= charttype, symbol=symbol, startdate=startdate, enddate=enddate )
 
-
+#render notAvailable template
 @app.route('/notFound')
 def notAvailable():
     return render_template('notAvailable.html')
